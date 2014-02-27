be809b2d5f9288a114f0f0a9d7e7731577340bf3
Claudemir <claudemir@bsd.com.br>
2014-02-27 09:35:42 -0300

    Funções Claudemir
    Funções de operador comentário é digito e é float.

--- analisadorlexico/src/main/Compilador.java
+++ analisadorlexico/src/main/Compilador.java
@@ -22,4 +22,266 @@ public class Compilador {
         //System.out.println(i);
     }
     
+    public static Boolean operador(String palavra) {
+
+        String estado = "q0";
+        char caracter = ' ';
+        int t = palavra.length();
+        int p = 0;
+
+        while (p < t) {
+            caracter = palavra.charAt(p);
+            if (caracter == ' ' || Character.isDigit(caracter)) {
+                estado = "qerror";
+            }
+
+            if (!Character.isDigit(caracter)) {
+                
+                if (caracter != '+' && caracter != '/' && caracter != '*' ){
+                    estado = "qerror";
+                }
+                
+            }
+
+            if (caracter == '+') {
+                switch (estado) {
+                    case "q0":
+                        estado = "q1";
+                        break;
+                    case "q1":
+                        estado = "q2";
+                        break;
+                    case "q2":
+                        estado = "qerror";
+                        break;
+                }
+            }
+
+            if (caracter == '/') {
+                switch (estado) {
+                    case "q0":
+                        estado = "q1";
+                        break;
+                    case "q1":
+                        estado = "qerror";
+                        break;
+                }
+            }
+
+            if (caracter == '*') {
+                switch (estado) {
+                    case "q0":
+                        estado = "q1";
+                        break;
+                    case "q1":
+                        estado = "q2";
+                        break;
+                    case "q2":
+                        estado = "qerror";
+                        break;
+                }
+            }
+            p++;
+        }
+
+        if (estado.equals("q1") || estado.equals("q2")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static Boolean comentarioLinha(String palavra) {
+
+        String estado = "q0";
+        char caracter = ' ';
+        int t = palavra.length();
+        int p = 0;
+
+        while (p < t) {
+            caracter = palavra.charAt(p);
+
+            if (caracter == '/') {
+                switch (estado) {
+                    case "q0":
+                        estado = "q1";
+                        break;
+                    case "q1":
+                        estado = "q2";
+                        break;
+                }
+            }
+
+            if (caracter != '\n') {
+                switch (estado) {
+                    case "q0":
+                        estado = "q0";
+                        break;
+                    case "q1":
+                        estado = "q1";
+                        break;
+                    case "q2":
+                        estado = "q2";
+                        break;
+                }
+            }
+
+            if (caracter == '\n') {
+                switch (estado) {
+                    case "q2":
+                        estado = "q3";
+                        break;
+                }
+            }
+            p++;
+        }
+        if (estado == "q3") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static Boolean comentarioMultiplo(String palavra) {
+
+        String estado = "q0";
+        char caracter = ' ';
+        int t = palavra.length();
+        int p = 0;
+
+        while (p < t) {
+            caracter = palavra.charAt(p);
+
+            if (caracter == '/') {
+                switch (estado) {
+                    case "q0":
+                        estado = "q1";
+                        break;
+                    case "q3":
+                        estado = "q4";
+                        break;
+                }
+            }
+
+            if (caracter == '*') {
+
+                switch (estado) {
+                    case "q1":
+                        estado = "q2";
+                        break;
+                    case "q2":
+                        estado = "q3";
+                        break;
+                }
+            }
+
+            if (caracter != '/' && caracter != '*') {
+                switch (estado) {
+                    case "q2":
+                        estado = "q2";
+                        break;
+                }
+            }
+
+            p++;
+        }
+        if (estado.equals("q4")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static Boolean ehDigit(String palavra) {
+
+        String estado = "q0";
+        char caracter = ' ';
+        int t = palavra.length();
+        int p = 0;
+
+        while (p < t) {
+            caracter = palavra.charAt(p);
+
+            if (Character.isDigit(caracter)) {
+                switch (estado) {
+                    case "q0":
+                        estado = "q1";
+                        break;
+                    case "q1":
+                        estado = "q1";
+                        break;
+                }
+            }
+
+            if (!Character.isDigit(caracter)) {
+                estado = "qerror";
+            }
+
+            if (caracter == ' ') {
+                estado = "qerror";
+            }
+
+            p++;
+        }
+        if (estado.equals("q1")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static Boolean ehFloat(String palavra) {
+
+        String estado = "q0";
+        char caracter = ' ';
+        int t = palavra.length();
+        int p = 0;
+
+        while (p < t) {
+            caracter = palavra.charAt(p);
+
+            if (Character.isDigit(caracter)) {
+                switch (estado) {
+                    case "q0":
+                        estado = "q1";
+                        break;
+                    case "q1":
+                        estado = "q1";
+                        break;
+                    case "q2":
+                        estado = "q3";
+                        break;
+                    case "q3":
+                        estado = "q3";
+                        break;
+                }
+            }
+
+            if (caracter == '.') {
+                switch (estado) {
+                    case "q1":
+                        estado = "q2";
+                        break;
+                    case "q2":
+                        estado = "qerror";
+                        break;
+                }
+            }
+
+            if (!Character.isDigit(caracter) && caracter != '.') {
+                estado = "qerror";
+            }
+
+            if (caracter == ' ') {
+                estado = "qerror";
+            }
+            p++;
+        }
+        if (estado.equals("q3")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
 }
